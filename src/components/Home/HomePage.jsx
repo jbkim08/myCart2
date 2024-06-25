a5716e321edb7ddd5075203a57bddd4836bea427
JBKIM <drv98@naver.com>
2024-06-25 14:59:59 +0900

    홈페이지 히어로섹션

---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./HeroSection";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection />
+      {/* 상품들 */}
+      {/* 히어로 섹션 */}
+    </div>
+  );
+};
+
+export default HomePage;
