f7368e558aa14693dc1808d56168fe5aab73edde
JBKIM <drv98@naver.com>
2024-06-26 10:27:54 +0900

    유즈레퍼런스

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,6 +1,8 @@
+import { useRef } from "react";
 import "./LoginPage.css";
 
 const LoginPage = () => {
+  const passwordRef = useRef(null);
   return (
     <section className="align_center form_page">
       <form className="authentication_form">
@@ -18,11 +20,24 @@ const LoginPage = () => {
           <div>
             <label htmlFor="password">Password</label>
             <input
+              ref={passwordRef}
               type="password"
               id="password"
               className="form_text_input"
               placeholder="패스워드"
             />
+            <button
+              type="button"
+              onClick={() => (passwordRef.current.type = "password")}
+            >
+              비밀번호 숨기기
+            </button>
+            <button
+              type="button"
+              onClick={() => (passwordRef.current.type = "text")}
+            >
+              비밀번호 보이게
+            </button>
           </div>
 
           <button type="submit" className="search_button form_submit">
