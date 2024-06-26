7e07ca816a67371d4a853096778911db2ebf9da4
JBKIM <drv98@naver.com>
2024-06-26 10:33:21 +0900

    로그인폼

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import "./LoginPage.css";
 
 const LoginPage = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({ email: "", password: "" });
+  };
+
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form onSubmit={handleSubmit} className="authentication_form">
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
               type="email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
               id="email"
               className="form_text_input"
               placeholder="이메일 입력..."
@@ -19,6 +33,8 @@ const LoginPage = () => {
             <label htmlFor="password">Password</label>
             <input
               type="password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               id="password"
               className="form_text_input"
               placeholder="패스워드"
