891ef54fe36898554ad18a2ff7db0a76d81611c4
JBKIM <drv98@naver.com>
2024-06-27 11:28:46 +0900

    제품싱글페이지

--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -1,63 +1,59 @@
 import { useState } from "react";
 import "./SingleProductPage.css";
 import QuantityInput from "./QuantityInput";
-
-//샘플 제품 객체
-const product = {
-  id: 1,
-  title: "상품 타이틀",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
-  price: 9900,
-  images: [
-    "https://via.placeholder.com/500x500?text=Product+Image+1",
-    "https://via.placeholder.com/500x500?text=Product+Image+2",
-    "https://via.placeholder.com/500x500?text=Product+Image+3",
-    "https://via.placeholder.com/500x500?text=Product+Image+4",
-  ],
-  stock: 10,
-};
+import { useParams } from "react-router-dom";
+import useData from "../../Hook/useData";
 
 const SingleProductPage = () => {
   //선택한 이미지 기억 (선택한 이미지 인덱스 번호를 저장)
   const [selectedImage, setSelectedImage] = useState(0);
+  const { id } = useParams(); //주소변수값 id를 얻기
+  //console.log(id);
+  //id값으로 제품 데이터 요청
+  const { data: product, error, isLoading } = useData(`products/${id}`);
+  //console.log(product);
   return (
     <section className="align_center single_product">
-      <div className="align_center">
-        <div className="single_product_thumbnails">
-          {product.images.map((image, index) => (
+      {error && <em className="form_error">{error}</em>}
+      {product._id && (
+        <>
+          <div className="align_center">
+            <div className="single_product_thumbnails">
+              {product.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={`http://localhost:5000/products/${image}`}
+                  alt={product.title}
+                  className={selectedImage === index ? "selected_image" : ""}
+                  onClick={() => setSelectedImage(index)}
+                />
+              ))}
+            </div>
+
+            {/* 큰 이미지는 왼쪽의 4개의 이미지중 선택한 인덱스 번호으 이미지 표시 */}
             <img
-              key={index}
-              src={image}
+              src={`http://localhost:5000/products/${product.images[selectedImage]}`}
               alt={product.title}
-              className={selectedImage === index ? "selected_image" : ""}
-              onClick={() => setSelectedImage(index)}
+              className="single_product_display"
             />
-          ))}
-        </div>
+          </div>
 
-        {/* 큰 이미지는 왼쪽의 4개의 이미지중 선택한 인덱스 번호으 이미지 표시 */}
-        <img
-          src={product.images[selectedImage]}
-          alt={product.title}
-          className="single_product_display"
-        />
-      </div>
+          <div className="single_product_details">
+            <h1 className="single_product_title">{product.title}</h1>
+            <p className="single_product_description">{product.description}</p>
+            <p className="single_product_price">
+              ￦ {product.price.toLocaleString("ko-KR")} 원
+            </p>
 
-      <div className="single_product_details">
-        <h1 className="single_product_title">{product.title}</h1>
-        <p className="single_product_description">{product.description}</p>
-        <p className="single_product_price">
-          ￦ {product.price.toLocaleString("ko-KR")} 원
-        </p>
+            <h2 className="quantity_title">구매개수:</h2>
+            <div className="align_center quantity_input">
+              <QuantityInput />
+            </div>
 
-        <h2 className="quantity_title">구매개수:</h2>
-        <div className="align_center quantity_input">
-          <QuantityInput />
-        </div>
-
-        <button className="search_button add_cart">장바구니 추가</button>
-      </div>
+            <button className="search_button add_cart">장바구니 추가</button>
+          </div>
+        </>
+      )}
     </section>
   );
 };
