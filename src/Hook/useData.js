11117f4c41174579b128f120b9cf9d41c4ab9b35
JBKIM <drv98@naver.com>
2024-06-27 09:40:40 +0900

    로딩상태 추가

--- src/Hook/useData.js
+++ src/Hook/useData.js
@@ -2,18 +2,27 @@ import { useEffect, useState } from "react";
 import apiClient from "../utils/api-client";
 
 //url에 서버의 뒷주소(endpoint)를 적어서 GET으로 데이터를 가져옴
+//현재 로딩상태를 추가
 const useData = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     apiClient
       .get(url)
-      .then((res) => setData(res.data))
-      .catch((err) => setError(err.message));
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
   }, []);
 
-  return { data, error };
+  return { data, error, isLoading };
 };
 
 export default useData;
