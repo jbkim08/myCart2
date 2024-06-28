23873b441a3eb0ea9df0498dc75370d273800779
JBKIM <drv98@naver.com>
2024-06-28 10:27:49 +0900

    카트데이터 가져오는 API 추가

--- src/services/cartServices.js
+++ src/services/cartServices.js
@@ -4,3 +4,8 @@ export function addToCartAPI(id, quantity) {
   //제품 id와 수량을 입력하여 장바구니 추가
   return apiClient.post(`/cart/${id}`, { quantity });
 }
+
+//카트 데이터를 가져옴
+export async function getCartAPI() {
+  return await apiClient.get("/cart");
+}
