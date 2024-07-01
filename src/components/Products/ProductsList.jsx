c4368260d85c97d517eb56bd6b13c0c702228302
JBKIM <drv98@naver.com>
2024-07-01 09:48:41 +0900

    정렬하기

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -4,8 +4,11 @@ import useData from "../../Hook/useData";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 import { useSearchParams } from "react-router-dom";
 import Pagination from "../Common/Pagination";
+import { useEffect, useState } from "react";
 
 const ProductsList = () => {
+  const [sortBy, setSortBy] = useState("");
+  const [sortedProducts, setSortedProducts] = useState([]);
   const [search, setSearch] = useSearchParams(); //?뒤의 쿼리스트링 가져옴
   const category = search.get("category"); //쿼리스트링에서 category=값을 가져옴
   const page = search.get("page");
@@ -25,11 +28,38 @@ const ProductsList = () => {
     setSearch({ ...currentParams, page: page });
   };
 
+  useEffect(() => {
+    if (data && data.products) {
+      const products = [...data.products]; //제품데이터 복사
+
+      if (sortBy === "price desc") {
+        setSortedProducts(products.sort((a, b) => b.price - a.price)); //가격 큰순
+      } else if (sortBy === "price asc") {
+        setSortedProducts(products.sort((a, b) => a.price - b.price)); //가격 작은순
+      } else if (sortBy === "rate desc") {
+        setSortedProducts(
+          products.sort((a, b) => b.reviews.rate - a.reviews.rate)
+        ); //평점 큰순
+      } else if (sortBy === "rate asc") {
+        setSortedProducts(
+          products.sort((a, b) => a.reviews.rate - b.reviews.rate)
+        ); //평점 작은순
+      } else {
+        setSortedProducts(products);
+      }
+    }
+  }, [sortBy, data]);
+
   return (
     <section className="products_list_section">
       <header className="align_center products_list_header">
         <h2>상품목록</h2>
-        <select name="sort" id="" className="products_sorting">
+        <select
+          onChange={(e) => setSortBy(e.target.value)}
+          name="sort"
+          id=""
+          className="products_sorting"
+        >
           <option value="">정렬방법</option>
           <option value="price desc">가격높은순</option>
           <option value="price asc">가격낮은순</option>
@@ -42,7 +72,7 @@ const ProductsList = () => {
         {error && <em className="form_error">{error}</em>}
         {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
         {data.products &&
-          data.products.map((product) => (
+          sortedProducts.map((product) => (
             <ProductCard key={product._id} product={product} />
           ))}
       </div>
